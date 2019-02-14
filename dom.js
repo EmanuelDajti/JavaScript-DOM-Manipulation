6fb5195ab02b15925d47522d2d6c00ec2ce9529d
Emanuel Dajti <dajti49@gmail.com>
2019-02-15 00:00:14 +0100

    new commit

--- dom.js
+++ dom.js
@@ -96,11 +96,93 @@ for(var i = 0; i < odd.length; i++) {
     odd[i].style.backgroundColor = '#f4f4f4';
     even[i].style.backgroundColor = '#ccc';
 }
-*/
+
 // TRAVERSING THE DOM //
 
 var itemList = document.querySelector('#items');
 
 //parentnode
 
-console.log(itemList.parentNode);
+//console.log(itemList.parentNode);
+
+itemList.parentNode.style.backgroundColor = '#f4f4f4';
+console.log(itemList.parentNode.parentNode);
+
+console.log(itemList.parentElement);
+itemList.parentElement.style.backgroundColor = '#f4f4f4';
+console.log(itemList.parentElement.parentElement);
+
+
+//childnodes
+
+
+var itemList = document.querySelector('#items');
+
+console.log(itemList.childNodes);
+
+
+
+var itemList = document.querySelector('#items');
+
+console.log(itemList.children);
+
+
+var itemList = document.querySelector('#items');
+//FirstChild
+console.log(itemList.firstChild);
+
+
+
+var itemList = document.querySelector('#items');
+
+console.log(itemList.firstElementChild);
+
+itemList.firstElementChild.textContent = 'Hello 1';
+
+
+
+// NEXTSIBLING
+
+var itemList = document.querySelector('#items');
+
+console.log(itemList.nextElementSibling);
+
+//previous sibling
+
+console.log(itemList.previousElementSibling);
+
+*/
+
+// createElement
+
+//Create a div
+
+var newDiv = document.createElement('div');
+
+//Add class
+
+newDiv.className = 'hello';
+
+// Add ID
+
+newDiv.id = 'hello1';
+
+//add attribute
+
+newDiv.setAttribute('title', 'Hello Developer');
+
+//Create a text node
+var newDivText = document.createTextNode('Hello World');
+
+//Add text to div
+newDiv.appendChild(newDivText);
+
+var container = document.querySelector('header .container');
+var h1 = document.querySelector('header h1');
+
+
+console.log(newDiv);
+
+
+
+container.insertBefore(newDiv, h1);
