f83b61d71dd2a4f0e902996eaca0885649d67930
Emanuel Dajti <dajti49@gmail.com>
2019-02-17 17:34:41 +0100

    Add Commit

--- dom.js
+++ dom.js
@@ -208,3 +208,50 @@ function buttonclick(e) {
 }
 
 */
+
+var button = document.getElementById('button');
+var box = document.getElementById('box');
+
+//button.addEventListener('click', runEvent);
+//button.addEventListener('dblclick', runEvent);
+//button.addEventListener('mousedown', runEvent);
+//button.addEventListener('mouseup', runEvent);
+
+//box.addEventListener('mouseenter', runEvent);
+//box.addEventListener('mouseleave', runEvent);
+
+//box.addEventListener('mouseover', runEvent);
+//box.addEventListener('mouseout', runEvent);
+
+//box.addEventListener('mousemove', runEvent);
+/*
+var itemInput = document.querySelector('input[type="text"]');
+var form = document.querySelector('form');
+var select = document.querySelector('select');
+
+select.addEventListener('change', runEvent);
+
+//itemInput.addEventListener('keydown', runEvent);
+
+//itemInput.addEventListener('focus', runEvent);
+// itemInput.addEventListener('blur', runEvent);
+
+//itemInput.addEventListener('cut', runEvent);
+//itemInput.addEventListener('paste', runEvent);
+
+form.addEventListener('submit', runEvent);
+
+
+function runEvent(e) {
+    e.preventDefault();
+    console.log('Event Type: '+e.type);
+   // console.log(e.target.value);
+   // document.getElementById('output').innerHTML = '<h3>'+e.target.value+'</h3>';
+   // output.innerHTML = '<h3>MouseX: '+e.offsetX+'</h3><h3>MouseY: '+e.offsetY+'</h3>';  
+   //box.style.backgroundColor = "rgb("+e.offsetX+", "+e.offsetY+", 40)";
+   // document.body.style.backgroundColor =  "rgb("+e.offsetX+", "+e.offsetY+", 40)";
+
+}
+
+*/
+
